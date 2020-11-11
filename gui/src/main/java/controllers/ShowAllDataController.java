f4d7a1929c1e4818c9903006b6d8e07d1c221508
andrijana.kulezic <andrijana.kulezic@gmail.com>
2020-11-11 16:59:09 +0100

    all data controller

--- gui/src/main/java/controllers/ShowAllDataController.java
+++ gui/src/main/java/controllers/ShowAllDataController.java
@@ -1,11 +1,17 @@
 package controllers;
 
+import appcore.MainFrame;
+import model.Entity;
+import service.FilterSortService;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class ShowAllDataController implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        List<Entity> allEntities = MainFrame.getInstance().getFilterSortService().getAllData();
+        MainFrame.getInstance().getDataTableModel().updateTableModel(allEntities);
     }
 }
